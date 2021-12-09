65b782c9f91fdeaa5c1d6cc2c5c23c5d77767667
BlingBong <rivano.atk@gmail.com>
2021-12-09 08:00:03 +0700

    uploadnya lama

---
+++ resources/views/admin/showproduct.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    @include('admin.sidebar')
+      <!-- partial -->
+      @include('admin.navbar')
+        <div style="padding-bottom: 30px" class="container-fluid page-body-wrapper">
+            <div class="container" align="center">
+                @if(session()->has('message'))
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert">x</button>
+                        {{session()->get('message')}}
+                    </div>
+                @endif
+                <table>
+                    <tr style="background-color: grey;">
+                        <td style="padding: 20px;">Title</td>
+                        <td style="padding: 20px;">Desctiption</td>
+                        <td style="padding: 20px;">Quantity</td>
+                        <td style="padding: 20px;">Price</td>
+                        <td style="padding: 20px;">Image</td>
+                        <td style="padding: 20px;">Update</td>
+                        <td style="padding: 20px;">Delete</td>
+                    </tr>
+                    @foreach($data as $product)
+                    <tr align="center" style="background-color: black;">
+                        <td>{{$product->title}}</td>
+                        <td>{{$product->description}}</td>
+                        <td>{{$product->quantity}}</td>
+                        <td>Rp.{{number_format($product->price,2,',','.');}}</td>
+                        <td>
+                            <img height="100px" width="100px" src="/productimage/{{$product->image}}">
+                        </td>
+                        <td>
+                            <a class="btn btn-primary" href="{{url('updateview',$product->id)}}">Update</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-danger" onclick="return confirm('Are you sure you want to delete?')" href="{{url('deleteproduct',$product->id)}}">Delete</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+        <!-- partial -->
+        @include('admin.script')
+  </body>
+</html>
